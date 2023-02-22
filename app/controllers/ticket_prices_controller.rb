8875ba958f2d98a7ed45cecb8e570b3df1a98525
Mucahid Canbey <mucahid.canbey@gmail.com>
2023-02-22 15:14:26 +0300

    Veri çekme işlemi

--- app/controllers/ticket_prices_controller.rb
+++ app/controllers/ticket_prices_controller.rb
@@ -1,4 +1,11 @@
 class TicketPricesController < ApplicationController
   def index
+    url = 'https://api.ibb.gov.tr/MetroIstanbul/api/MetroMobile/V2/GetTicketPrice/tr'
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    data = JSON.parse(response)
+
+    @ticket_prices = data['Data']
   end
+  
 end
